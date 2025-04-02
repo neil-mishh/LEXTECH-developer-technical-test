7ee5e49dada8ef15f746826d2c6c2851a1b7f386
neil_mishh <thensm9neil@gmail.com>
2025-04-03 00:28:10 +1300

    Add basic code template to main code file

--- LEXTECH-test/Program.cs
+++ LEXTECH-test/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+}
