38fb0e080a9530b06054221f948cd76cda521a6b
neil_mishh <thensm9neil@gmail.com>
2025-04-03 01:29:48 +1300

    Create interface

---
+++ LEXTECH-test/Interfaces/IMissingNumber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LEXTECH_test.Interfaces
+{
+    public interface IMissingNumber
+    {
+        int GetExpectedSum(int n);
+        int GetCurrentSum(int[] input);
+        int GetMissingNum(int[] input);
+    }
+}
